ce0ef99cb58f2f65d80c2723e1b4c07f891c678d
Maddy <90936389+SethPlattner@users.noreply.github.com>
2022-01-31 16:46:29 -0800

    im tired ill finish this later

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -6,10 +6,11 @@ import java.io.Console;
 
 import edu.wpi.first.wpilibj.Joystick;
 import frc.robot.Constants.RobotMap;
-
+// ask whoever the operator ends up being what button they want the intake to be
 public class OI {
     private Joystick driveStick1 = new Joystick(RobotMap.JOYSTICK_PORT1); // this is the joystick for movement
     private Joystick driveStick2 = new Joystick(RobotMap.JOYSTICK_PORT2); // this joystick is for buttons
+    
     public double getJoystick1RawAxis(int axis){
         //System.out.println("axis: " + axis);
         return driveStick1.getRawAxis(Math.abs(axis - 1)); // Math.abs = big hack to invert axes. Note: literally does not work with 2. If not working take away Callum's granola bar eating privileges
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,7 +1,7 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-
+// note please ask whoever the operator ends up being what they want to press for intake
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
@@ -19,10 +19,7 @@ public class Intake extends SubsystemBase {
     currentIntakeStatus = false;
   }
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+  
   public void intakeRunning(boolean intakeOn){
     if (intakeOn != currentIntakeStatus){
       if(intakeOn == true){
