4502116a56686db85ba2a3c5382c9234f2873a74
Maddy <90936389+SethPlattner@users.noreply.github.com>
2022-02-01 15:12:16 -0800

    expanded OI, increased readability

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -2,20 +2,27 @@
 // this is our operator interface class, this is where we have all the code that lets buttons and parts of the joysticks be used
 package frc.robot;
 
-import java.io.Console;
+//import java.io.Console;
+//import edu.wpi.first.wpilibj.Joystick;
+// i changed it to a generic HID instead of joystick to make code easier to compare to docs
+import edu.wpi.first.wpilibj.GenericHID;
 
-import edu.wpi.first.wpilibj.Joystick;
 import frc.robot.Constants.RobotMap;
 // ask whoever the operator ends up being what button they want the intake to be
 public class OI {
-    private Joystick driveStick1 = new Joystick(RobotMap.JOYSTICK_PORT1); // this is the joystick for movement
-    private Joystick driveStick2 = new Joystick(RobotMap.JOYSTICK_PORT2); // this joystick is for buttons
+    private GenericHID driveStick = new GenericHID(RobotMap.JOYSTICK_PORT1); // this is the joystick for movement
+    private GenericHID operatorStick = new GenericHID(RobotMap.JOYSTICK_PORT2); // this joystick is for buttons
     
-    public double getJoystick1RawAxis(int axis){
+    public double getDriveStickRawAxis(int axis){
         //System.out.println("axis: " + axis);
-        return driveStick1.getRawAxis(Math.abs(axis - 1)); // Math.abs = big hack to invert axes. Note: literally does not work with 2. If not working take away Callum's granola bar eating privileges
+        return driveStick.getRawAxis(Math.abs(axis - 1)); // Math.abs = big hack to invert axes. Note: literally does not work with 2. If not working take away Callum's granola bar eating privileges
     }
-    
+    public boolean getOperatorStickButton(int button){
+        return operatorStick.getRawButton(button);
+    }
+    public int getDriveStickSliderAxis(){
+        return driveStick.getAxisCount() -1;
+    }
   
 }
 
