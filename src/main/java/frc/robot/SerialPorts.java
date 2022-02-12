c25dc3b769211269f51e4d64d305a9a12e1c6695
Maddy <90936389+SethPlattner@users.noreply.github.com>
2022-02-12 11:34:27 -0800

    added light, now prints all available serial ports

---
+++ src/main/java/frc/robot/SerialPorts.java
@@ -0,0 +1,16 @@
+package frc.robot;
+
+import java.util.List;
+
+public class SerialPorts {
+    private DatumSerial datumSerial;
+    public SerialPorts(){
+        datumSerial = new DatumSerial();
+        List<String> portNames= datumSerial.getPorts();
+        System.out.println("Available Ports:");
+        
+        for( int i = 0; 1<portNames.size(); i++){
+            System.out.println(portNames.get(i));
+        }
+    }
+}
