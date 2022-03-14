95df9a4f34ffb6a6f4a93f6f9dafb63e1ca17708
Irishumanoid <95321751+Irishumanoid@users.noreply.github.com>
2022-03-14 13:27:06 -0700

    Update RobotArm.java

--- src/main/java/frc/robot/subsystems/RobotArm.java
+++ src/main/java/frc/robot/subsystems/RobotArm.java
@@ -1,83 +1,101 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 package frc.robot.subsystems;
-
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMax.IdleMode;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
-
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.Timer;
-import frc.robot.Constants.RobotMap;
-
-public class RobotArm extends RobotArmBase {
-
-  private double offsetAngle; //in degrees
-  private final static double kPArm = 0.1;
-  private final static double kIArm = 0.01;
-  private final static double kDArm = 0.5;
-  private final static double errorLimit = 1.5;
+import frc.robot.subsystems.abstractSubsystems.RobotArmBase;
+// i know that this might break things but i cant help but have a little fun with abstraction as it also allows for easier tweaking of arm subsystems at competion without breaking code
+public class RobotArmPID extends RobotArmBase {
+  /** Creates a new robotArm. */
   
- 
+  /*private double upperAngle;
+  private double lowerAngle;*/
+  private final static double kPArm = 0.1; 
+  private final static double kIArm = 0.01;
+  private final static double kDArm = 0.3;
+  private final static double errorLimit = 1.5;
+  protected Timer armTimer;
   private static double error = 0;
-  private static double errorSum = 0;
+  private static double errorSum = 0; 
   private static double lastError = 0;
-  private static double lastTimestamp = 0;
-  private static double setpoint = 0; 
-  private static double encoderInit = 0; 
-  private static double lastTimestamp = Timer.getFPGATimestamp();
-  private static double encoderInit = armMotor.getEncoder().getPosition()*360;
- 
+  private static double setpoint = 0;
+  private double encoderInit = armMotor.getEncoder().getPosition(); 
+  private double encoderPosition = encoderInit;
+  private double lastTimestamp = armTimer.get();
+  private double newSpeed = 0;
   
   @Override
+  protected void otherConfigs() {
+    armTimer= new Timer();
+    armTimer.start();
+  }
+@Override
   public void periodic() {
-      System.out.println("arm encoder position" + armMotor.getEncoder().getPosition());
-      //returns units in #rotations, which is converted to degrees
-      double encoderPosition = encoderInit + armMotor.getEncoder().getPosition()*360;
-
-      if (armMotor.get() > 0) {
-          setpoint = 90;
-      } else if (armMotor.get() < 0) {
-          setpoint = 0;
-      }
-
-      //as arm approaches setpoint, it will slow down
+      encoderPosition += armMotor.getEncoder().getPosition()*360/80;
       error = setpoint - encoderPosition;
-      double dt = (Timer.getFPGATimestamp() - lastTimestamp);
-
+      double dt = (armTimer.get()-lastTimestamp);
       if (Math.abs(error) < errorLimit) {
         errorSum += error*dt;
+      } 
+      
+      double errorRate = (error-lastError)/dt;
+      newSpeed = kPArm*error + kIArm*errorSum + kDArm*errorRate;
+      if (newSpeed > 0.3) {
+          newSpeed = 0.3;
       }
-
-
-      double errorRate = (error - lastError)/dt;
-      double newSpeed = kPArm*error + kIArm*errorSum + kDArm*errorRate;
-
-      if(Math.abs(setpoint-encoderPosition) < 0.1 && armMotor.get() < 0) {
-        armMotor.set() = 0;
+      if (Math.abs(setpoint-encoderPosition) < 0.1 && armMotor.get() < 0) {
+          armMotor.set(0);
+      } else {
+        armMotor.set(newSpeed);
       }
-
-      armMotor.set(newSpeed);
+      
       lastError = error;
-      lastTimestamp = Timer.getFPGATimestamp();
+      lastTimestamp = armTimer.get();
+      
   }
-
-  public void raiseArm() {
+    
+  @Override
+  protected void raiseArm(double maxArmPower) {
+    
+    System.out.println("robot arm raise called");
+    setpoint = 75;
     armMotor.set(newSpeed);
   }
-
-  public void lowerArm()  {
-    armMotor.set(-newSpeed);
-
-    try {
-      Thread.sleep(100);
-    } catch (InterruptedException e) {
-      System.out.println("arm wait didn't work");
+  @Override
+  protected void lowerArm(double maxArmPower)  {
+  
+    System.out.println("robot arm lower called");
+    setpoint = -75;
+    armMotor.set(-newSpeed); 
+  }
+  @Override
+  public void moveArm(boolean ArmUp) {
+    if (ArmUp) {
+      raiseArm(.3);
+      if(!armStatus){
+        armTimer.reset();
+        armMotor.getEncoder().setPosition(0);
+      }
+      armStatus = true;
+    } else {
+      lowerArm(-.1);
+      if(armStatus){
+        armTimer.reset();
+        armMotor.getEncoder().setPosition(0);
+      }
+      armStatus = false;
     }
-    while(armMotor.getEncoder().getVelocity() < 0){
-      //System.out.println("arm lowering");
-    }
-    armMotor.set(0);
     
   }
+  @Override
+  public void moveArmFirst() {
+    lowerArm(-.3);
+    if(armStatus){
+      armTimer.reset();
+    }
+    armStatus = false;
+    System.out.println("auto lower called");
+  }
+  
   
 }
