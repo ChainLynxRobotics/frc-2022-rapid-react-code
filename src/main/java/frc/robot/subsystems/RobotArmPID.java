a83d199ff87bff1409b11cdd915b62b13a9d41cc
SethPlattner <90936389+SethPlattner@users.noreply.github.com>
2022-04-01 11:45:19 -0700

    Update RobotArmPID.java

--- src/main/java/frc/robot/subsystems/RobotArmPID.java
+++ src/main/java/frc/robot/subsystems/RobotArmPID.java
@@ -1,120 +1,95 @@
 package frc.robot.subsystems;
 
-import java.lang.Math;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import frc.robot.subsystems.abstractSubsystems.RobotArmBase;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
+
 
 public class RobotArmPID extends RobotArmBase {
-  /*private double upperAngle;
-  private double lowerAngle;*/
-  public final static double kPArm = 0.1; 
-  public final static double kIArm = 0.01;
-  public final static double kDArm = 0.3;
-  private final static double errorLimit = 1.5;
-  protected Timer armTimer;
-  private static double error = 0;
-  private static double errorSum = 0; 
-  private static double lastError = 0;
-  private static double setpoint = 0;
-  private double encoderInit = armMotor.getEncoder().getPosition(); 
-  private double encoderPosition = encoderInit;
-  private double lastTimestamp = armTimer.get();
-  private double newSpeed = 0;
-  private double armSpeed = 0;
-  private double maxArmPower;
-
-  //private ShuffleboardTab PIDTab = ShuffleBoard.getTab("PIDArm");
-  //private NetworkTableEntry angularSpeedEntry = PIDTab.add("SpeedVelocity", 0).getEntry(); //use in periodic
-  
-  @Override
+    protected Timer armTimer;
+    public static final double kPArm = 0.1;
+    public static final double kIArm = 0;
+    public static final double kDArm = 0;
+    public static final double errorLimit = 1.5;
+    private static double error = 0;
+    private static double errorSum = 0;
+    private static double lastError = 0;
+    private static double setpoint = 75; //down position
+    private double encoderInit = 0;
+    private double encoderPosition = encoderInit;
+    private double lastTimestamp = armTimer.get();
+    private double newSpeed = 0;
+    private double dt = 0;
+    private double errorRate = 0;
+ 
+    @Override
   protected void otherConfigs() {
-    armTimer = new Timer();
+    armTimer= new Timer();
     armTimer.start();
   }
-  
-  public double getkP() {
-    return kPArm;
-  }
-  
-  public double getkI() {
-    return kIArm;
-  }
-  
-  public double getkD() {
-    return kDArm;
-  }
-
-@Override
-  public void periodic() {
-
-      encoderPosition += armMotor.getEncoder().getPosition()*360/80;
-      error = setpoint - encoderPosition;
-      double dt = (armTimer.get()-lastTimestamp);
-      if (Math.abs(error) < errorLimit) {
-        errorSum += error*dt;
-      } 
-      
-      double errorRate = (error-lastError)/dt;
-      newSpeed = kPArm*error + kIArm*errorSum + kDArm*errorRate;
-      if (newSpeed > 0.3) {
-          newSpeed = 0.3;
-      }
-      if (Math.abs(setpoint-encoderPosition) < 0.1 && armMotor.get() < 0) {
-          armMotor.set(0);
-      } else {
-        armMotor.set(newSpeed);
-      }
-
-      //Kv*v*2pi*newSpeed/60 sec
-      armSpeed = 473*12*2*Math.PI*newSpeed/60;
-
-      //SmartDashboard.putNumber("angularSpeed", armSpeed);
-      //angularSpeedEntry.setDouble(armSpeed);
-
-      lastError = error;
-      lastTimestamp = armTimer.get();
-  }
     
+  @Override
+  public void periodic() {
+    encoderPosition += armMotor.getEncoder().getPosition()*360;
+    error = setpoint - encoderPosition;
+    dt = armTimer.get() - lastTimestamp;
+
+    if(Math.abs(error) < errorLimit) {
+        errorSum += error; 
+    }
+
+    errorRate = (error - lastError)/dt;
+    newSpeed = kPArm*error+kIArm*errorSum+kDArm*errorRate;
+
+    if (newSpeed > 0.3) {
+        newSpeed = 0.3;
+    }
+    
+    if(Math.abs(setpoint-encoderPosition) < 0.1 && armMotor.get() < 0) {
+        newSpeed = 0;
+    } else if(Math.abs(setpoint-encoderPosition) < 35 && armMotor.get() > 0) {
+        newSpeed = 0.1;
+    }
+
+    lastError = error;
+    lastTimestamp = armTimer.get();
+  }
+
+
   @Override
   protected void raiseArm() {
-    
+    setpoint = 0;
     System.out.println("robot arm raise called");
-    setpoint = 75;
-    armMotor.set(newSpeed);
+    armMotor.set(newSpeed); //.3 
+    
   }
-
   @Override
   protected void lowerArm()  {
+    setpoint = 75;
     System.out.println("robot arm lower called");
-    setpoint = -75;
-    armMotor.set(-newSpeed); 
+    armMotor.set(-newSpeed); //-.3
+    
+    
   }
 
   @Override
   public void moveArm(boolean ArmUp) {
+   
+    
     if (ArmUp) {
       raiseArm();
       if(!armStatus){
         armTimer.reset();
-        armMotor.getEncoder().setPosition(0);
       }
       armStatus = true;
     } else {
       lowerArm();
       if(armStatus){
         armTimer.reset();
-        armMotor.getEncoder().setPosition(0);
       }
       armStatus = false;
     }
     
   }
-  
 
-  
-  
+
 }
