82f819f4618f1d0ec1e1a799e09f57ace70ad72e
Ishan F.G. Coutinho <ishan.f.coutinho@gmail.com>
2022-04-07 14:10:42 -0700

    bonney lake code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -16,14 +16,13 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-//import frc.robot.Constants.DriveConstants;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.DriveStyle;
 import frc.robot.Constants.JoystickScaling;
 import frc.robot.subsystems.BallHandler;
 import frc.robot.subsystems.DriveTrain;
 
-import frc.robot.subsystems.RobotArm;
-import frc.robot.subsystems.RobotArmNew;
 import frc.robot.subsystems.RobotArm;
 import frc.robot.subsystems.abstractSubsystems.RobotArmBase;
 
@@ -65,13 +64,14 @@ public class RobotContainer {
   // this is where we will set up camera code
   private void configureCameras() {
     CameraServer.startAutomaticCapture("camera1",0);
+   // CameraServer.startAutomaticCapture("camera2",1);
   }
 
   // this is the method where we are going to start all our commands to reduce clutter in RobotContainer method
    private void startCommands() {
-    driveTrain.setDefaultCommand(new RunCommand(() ->driveTrain.drive(m_OI.getDriveStickRawAxis(0),(m_OI.getDriverButton(2)||m_OI.getDriverButton(1))?1:m_OI.getDriveStickRawAxis(1),getDriveMultiplier(),JoystickScaling.SQUARED_EXPONENTIAL,4,(m_OI.getDriverButton(7)?DriveStyle.ARCADE_TANK:DriveStyle.NORMAL_ARCADE)),  driveTrain)/*driveTrainChooser.getSelected()*/);
+    driveTrain.setDefaultCommand(new RunCommand(() ->driveTrain.drive(m_OI.getDriveStickRawAxis(0),(m_OI.getDriverButton(2)||m_OI.getDriverButton(1))?1:m_OI.getDriveStickRawAxis(1),getDriveMultiplier(),JoystickScaling.LINEAR,4,m_OI.getDriverButton(7)?DriveStyle.NORMAL_ARCADE:DriveStyle.NORMAL_ARCADE),  driveTrain)/*driveTrainChooser.getSelected()*/);
     // disabled operator commands as they threw an error when the operator joystick was not connected
-    ballHandler.setDefaultCommand(new RunCommand(() -> ballHandler.ballHandlerRunning(m_OI.getOperatorStickSliderAxis(),m_OI.getOperatorButton(1),m_OI.getOperatorButton(3),m_OI.getOperatorButton(2)),ballHandler));
+    ballHandler.setDefaultCommand(new RunCommand(() -> ballHandler.ballHandlerRunning(m_OI.getOperatorStickSliderAxis(),m_OI.getOperatorButton(1),m_OI.getDriverButton(2),m_OI.getOperatorButton(3)),ballHandler));
     robotArm.setDefaultCommand(new RunCommand(() -> robotArm.moveArm(m_OI.getOperatorButtons67Toggle()), robotArm));
    }
    // method to allow for constant multiplier for drivetrain speed
@@ -101,7 +101,7 @@ public class RobotContainer {
   
   public Command getAutonomousDriveCommand() {
     // this code should work but i am very skeptical of stuff like this so it might not
-    return new SequentialCommandGroup(new RunCommand(() -> ballHandler.ballHandlerRunning(.5,true,false,false),ballHandler).withTimeout(2),
+    return new SequentialCommandGroup(new WaitCommand(10),new RunCommand(() -> ballHandler.ballHandlerRunning(1,true,false,false),ballHandler).withTimeout(1),
     new RunCommand(() -> driveTrain.testDrive(-0.4, -0.4), driveTrain).withTimeout(3),new RunCommand(() -> robotArm.moveArm(true), robotArm));
     
   }
--- src/main/java/frc/robot/subsystems/abstractSubsystems/RobotArmBase.java
+++ src/main/java/frc/robot/subsystems/abstractSubsystems/RobotArmBase.java
@@ -8,19 +8,21 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import frc.robot.Constants.RobotMap;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 // extend this class if you want to make your own version of robot arm, this will make your life signifigantly easier
 public abstract class RobotArmBase extends SubsystemBase {
   /** Creates a new RobotArmBase. */
-  
+
   protected CANSparkMax armMotor;
   protected boolean armStatus;
+
   // you dont need to write a constructor, just another benifit
   public RobotArmBase() {
     armMotor = new CANSparkMax(RobotMap.ROBOT_ARM_MOTOR_ID, MotorType.kBrushless);
     armMotor.setIdleMode(IdleMode.kBrake);
     armMotor.setSmartCurrentLimit(40);
-    
 
     // change this to true after code is tested
     armStatus = true;
@@ -29,18 +31,21 @@ public abstract class RobotArmBase extends SubsystemBase {
 
   // use this method to set initial values for variables of an arm subsystem
   protected abstract void otherConfigs();
-  //here put code to run when the arm is getting lower/ is low
+
+  // here put code to run when the arm is getting lower/ is low
   protected abstract void lowerArm();
+
   // here put code to run when the arm is raising / is raised
   protected abstract void raiseArm();
+
   // this is the method that is called by other objects, DO NOT OVERRIDE
-  public abstract void moveArm(boolean ArmUp); 
-  
+  public abstract void moveArm(boolean ArmUp);
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
     System.out.println("motor temperature" + armMotor.getMotorTemperature());
-    
+    SmartDashboard.putNumber("status/armmotortemperature", armMotor.getMotorTemperature());
+
   }
 }
